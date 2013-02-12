90d4cce03d7e28c6f99ecb2ee7c450a4657eefeb
denise <djtcin@gmail.com>
2013-02-12 18:08:32 -0300

    Commit Inicial
    Estrutura do programa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskcenter::Application.config.secret_token = 'a7d4d15e1badf7ab0050c060a36c942f6e3b332bd818b6803025cc62d6db0116b0a359eae2ec3743f9568703ce34f74f2c180ab200971b02c86ab2b40f54f3a9'
